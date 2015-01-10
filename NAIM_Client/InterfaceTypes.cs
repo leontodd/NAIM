cbd1509bbaa64ec0af64e1f6613f739a953686f7
leontodd <leontodd97@gmail.com>
2015-01-10 22:43:10 +0000

    Changed folder structure

---
+++ NAIM_Client/InterfaceTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAIM_Client
+{
+    class Conversation
+    {
+        public Conversation(string _cid, string _user1, string _user2) { cid = _cid; user1 = _user1; user2 = _user2; }
+        public string cid, user1, user2;
+        public List<Message> messages = new List<Message>();
+    }
+
+    class Message
+    {
+        public Message(string _content, string _user, string _time) { content = _content; user = _user; time = _time; }
+        public string content, user, time;
+    };
+}
